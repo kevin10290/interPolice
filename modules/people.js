0309e7d6a45739aaa389ce12f4d12aac8340721f
kevin10290 <kevinalzate10290@gmail.com>
2024-02-28 11:13:52 -0500

    se actualiza people ahora podemos extraer la imgen hacia el front con multer

--- modules/people.js
+++ modules/people.js
@@ -5,95 +5,89 @@ const people = express.Router();
 const cnx = require("./bdata");
 const multer = require("multer");
 const fs = require("fs");
-const path = require("node:path")
-//configuracion del multer 
-//creacion del espacio de almacenamiento en el servidor 
+const path = require("node:path");
+//configuracion del multer
+//creacion del espacio de almacenamiento en el servidor
 const almacenamiento = multer.diskStorage({
-    //destination es una variable de multer para configurar el directorio de destino de la API 
-    //(recordar cerrar la ruta con el slash "/")
-    destination:  (req, file, cb)=> {
-      cb(null, './uploads/people/')
-    },
-    //configuracion de nombre del archivo a guardar en el disco duro de la API 
-    filename:  (req, file, cb)=> {
-      cb(null, "people" + '-' + Date.now()+file.originalname)
-    }
-  })
-  
-  const upload = multer({ storage: almacenamiento })
+  //destination es una variable de multer para configurar el directorio de destino de la API
+  //(recordar cerrar la ruta con el slash "/")
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/people/");
+  },
+  //configuracion de nombre del archivo a guardar en el disco duro de la API
+  filename: (req, file, cb) => {
+    cb(null, "people" + "-" + Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: almacenamiento });
 
 /* Desarrollo del crud */
 
 //Consultar
-people.get("/people/listing",(req,res)=>{
-    let sql = "SELECT * FROM people order by lastname";
+people.get("/people/listing", (req, res) => {
+  let sql = "SELECT * FROM people order by lastname";
 
-    cnx.query(sql,(error,data)=>{
-    try{
-        res.status(200).send(data)
-    }
-    catch (error){
-        console.log(error);
-/*         res.status(404).send({
+  cnx.query(sql, (error, data) => {
+    try {
+      res.status(200).send(data);
+    } catch (error) {
+      console.log(error);
+      /*         res.status(404).send({
             id:error.id,
             mensaje:error.message,
         }); */
     }
-    });
-})
+  });
+});
 
-//consultar por id 
-people.get("/people/listing/:id",(req,res)=>{
-    let id= req.params.id;
-    let sql = "SELECT * FROM people WHERE id="+id;
-    //cnx.query("SELECT * FROM people where id = ?",id,(error,data))
+//consultar por id
+people.get("/people/listing/:id", (req, res) => {
+  let id = req.params.id;
+  let sql = "SELECT * FROM people WHERE id=" + id;
+  //cnx.query("SELECT * FROM people where id = ?",id,(error,data))
 
-    cnx.query(sql,(error,data)=>{
-    try{
-        res.status(200).send(data)
-    }
-    catch (error){
-        console.log(error);
-        // throw `hay un error en la consulta: ${error}`
-/*         res.status(404).send({
+  cnx.query(sql, (error, data) => {
+    try {
+      res.status(200).send(data);
+    } catch (error) {
+      console.log(error);
+      // throw `hay un error en la consulta: ${error}`
+      /*         res.status(404).send({
             id:error.id,
             mensaje:error.message,
         }); */
     }
-    });
-})
+  });
+});
 
 //insertar una persona
-people.post("/people/create",(req,res)=>{
-    let frmData ={
-        name: req.body.name,
-        lastname: req.body.lastname,
-        nickname: req.body.nickname,
-        email: req.body.email,
-        type: req.body.type,
-    } 
-   
-    cnx.query("INSERT INTO people set ?",frmData,(error,data)=>{
-    try{
-        if(!error)
-        {
-            res.status(200).redirect('http://127.0.0.1:5500/api/index.html')
-        }
-        else{
-            res.status(500).send(error)
-        }
-       
-    }
-    catch (error){
-        console.log(error);
-        // throw `hay un error en la consulta: ${error}`
-/*         res.status(404).send({
+people.post("/people/create", (req, res) => {
+  let frmData = {
+    name: req.body.name,
+    lastname: req.body.lastname,
+    nickname: req.body.nickname,
+    email: req.body.email,
+    type: req.body.type,
+  };
+
+  cnx.query("INSERT INTO people set ?", frmData, (error, data) => {
+    try {
+      if (!error) {
+        res.status(200).redirect("http://127.0.0.1:5500/api/index.html");
+      } else {
+        res.status(500).send(error);
+      }
+    } catch (error) {
+      console.log(error);
+      // throw `hay un error en la consulta: ${error}`
+      /*         res.status(404).send({
             id:error.id,
             mensaje:error.message,
         }); */
     }
-    });
-})
+  });
+});
 
 /**
  * Description placeholder
@@ -105,109 +99,120 @@ people.post("/people/create",(req,res)=>{
  */
 
 //actualizar un registro
-people.put("/people/update/:id",
-(req,res)=>{
-    let id = req.params.id; //parametro
-    let frmData ={
-        name: req.body.name,
-        lastname: req.body.lastname,
-        nickname: req.body.nickname,
-        email: req.body.email,
-        type: req.body.type,
-    } 
-   
+people.put("/people/update/:id", (req, res) => {
+  let id = req.params.id; //parametro
+  let frmData = {
+    name: req.body.name,
+    lastname: req.body.lastname,
+    nickname: req.body.nickname,
+    email: req.body.email,
+    type: req.body.type,
+  };
 
-    
-    cnx.query("UPDATE people set ? WHERE id=?",[frmData,id],(error,data)=>{
-    try{
-        res.status(200).send("Actualización exitosa")
+  cnx.query("UPDATE people set ? WHERE id=?", [frmData, id], (error, data) => {
+    try {
+      res.status(200).send("Actualización exitosa");
+    } catch (error) {
+      console.log(error);
+      // throw `hay un error en la consulta: ${error}`
+      res.status(404).send({
+        id: error.id,
+        mensaje: error.message,
+      });
     }
-    catch (error){
-        console.log(error);
-        // throw `hay un error en la consulta: ${error}`
-        res.status(404).send({
-            id:error.id,
-            mensaje:error.message,
-        });
-    }
-    });
-})
-
-
-//subir imagen, tener en cuenta el middeleware de multer que configuramos en el objeto upload
-people.put("/people/uploadimage/:id",[upload.single("photo")],(req,res)=>{
-    //validacion 
-    // 1. que llegue un archivo 
-    if(!req.file && !req.files){
-        res.status(404).send({
-            status:"error",
-            message:"No existe el archivo"
-        });
-        return; // Agrega un retorno para evitar que el código siga ejecutándose
-    }
-
-    
-    //obtener la extension png , jpg , jpeg
-    let archivo = req.file.originalname;
-    let extension = archivo.split(".");
-    extension=extension[1];
-
-    if(extension != "png" && extension != "jpg" && extension != "jpeg"){
-
-        fs.unlink(req.file.path,(error)=>{
-            res.status(404).send({
-                status:"error",
-                message:"fallo el borrado"
-            });
-        });
-    
-      
-    }
-
-    //recibimos el parametro id 
-    let id = req.params.id;
-    //recibimos la imagen a subir
-    let photo = req.file.filename;
-    //ejecutamos la consulta de actualizacion de la imagen
-    cnx.query("UPDATE people set photo =? WHERE id = ?", [photo, id], (error, data) => {
-        if (error) {
-            res.status(404).send({
-                status: "error",
-                message: "Error de actualizacion",
-                details: error.message
-            });
-        }else{
-            res.status(200).send({
-                status: "ok",
-                message: "Actualizacion exitosa de la imagen"
-            });
-        }
-
-        
-    });
+  });
 });
 
+//subir imagen, tener en cuenta el middeleware de multer que configuramos en el objeto upload
+people.put("/people/uploadimage/:id", [upload.single("photo")], (req, res) => {
+  //validacion
+  // 1. que llegue un archivo
+  if (!req.file && !req.files) {
+    res.status(404).send({
+      status: "error",
+      message: "No existe el archivo",
+    });
+    return; // Agrega un retorno para evitar que el código siga ejecutándose
+  }
 
-//eliminar por id 
+  //obtener la extension png , jpg , jpeg
+  let archivo = req.file.originalname;
+  let extension = archivo.split(".");
+  extension = extension[1];
 
-people.delete("/people/deleteid/:id",(req,res)=>{
-    let id= req.params.id;
-    let sql = "DELETE FROM people WHERE id="+id;
-    //cnx.query("SELECT * FROM people where id = ?",id,(error,data))
+  if (extension != "png" && extension != "jpg" && extension != "jpeg") {
+    fs.unlink(req.file.path, (error) => {
+      res.status(404).send({
+        status: "error",
+        message: "fallo el borrado",
+      });
+    });
+  }
 
-    cnx.query(sql,(error,data)=>{
-    try{
-        res.status(200).send("Borrado Exitoso")
-    }catch (error){
-        console.log(error);
-        // throw `hay un error en la consulta: ${error}`
-/*         res.status(404).send({
+  //recibimos el parametro id
+  let id = req.params.id;
+  //recibimos la imagen a subir
+  let photo = req.file.filename;
+  //ejecutamos la consulta de actualizacion de la imagen
+  cnx.query(
+    "UPDATE people set photo =? WHERE id = ?",
+    [photo, id],
+    (error, data) => {
+      if (error) {
+        res.status(404).send({
+          status: "error",
+          message: "Error de actualizacion",
+          details: error.message,
+        });
+      } else {
+        res.status(200).send({
+          status: "ok",
+          message: "Actualizacion exitosa de la imagen",
+        });
+      }
+    }
+  );
+});
+
+//devolver la ruta de la imagen desde la API
+people.get("/people/sendimage/:photo", (req, res) => {
+  //parametro
+  let photo = req.params.photo;
+  //ruta
+    let ruta = "./uploads/people/"+photo;
+    fs.access(ruta,(error)=>{
+        if(!error){
+            res.sendFile(path.resolve(ruta))
+        }else{
+            res.status(404).send({
+                status: "error",
+                message: "no exite la imagen solicitada",
+              });
+        }
+    })
+  //acceso y retorna de la imagen usando su ruta
+ 
+});
+
+//eliminar por id
+
+people.delete("/people/deleteid/:id", (req, res) => {
+  let id = req.params.id;
+  let sql = "DELETE FROM people WHERE id=" + id;
+  //cnx.query("SELECT * FROM people where id = ?",id,(error,data))
+
+  cnx.query(sql, (error, data) => {
+    try {
+      res.status(200).send("Borrado Exitoso");
+    } catch (error) {
+      console.log(error);
+      // throw `hay un error en la consulta: ${error}`
+      /*         res.status(404).send({
             id:error.id,
             mensaje:error.message,
         }); */
     }
-    });
-})
+  });
+});
 
-
-module.exports = people; 
+module.exports = people;
